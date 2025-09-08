696ebdc3f0b0eeb11ca1b22713afc2ce6bcc5864
santos2607 <ssacchetti@mail.austral.edu.ar>
2025-09-08 12:53:34 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,9 @@
-# Replace the "ANSWER HERE" for your answer
-
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+    meses = [ "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+
+        if 1 <= month <= 12:
+            return meses [month - 1]
+
+        else:
+            return "error"
+      
