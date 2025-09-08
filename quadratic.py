e188120298bad39811868c16a19d4b1b27d8be36
santos2607 <ssacchetti@mail.austral.edu.ar>
2025-09-08 12:52:35 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,55 @@
-# Replace the "ANSWER HERE" for your answer
+import cmath
 
 def roots(a, b, c):
-    return "ANSWER HERE"
+    if a == 0:  
+        return "( )"
+
+    discriminante = cmath.sqrt(b**2 - 4*a*c)
+    r1 = (-b + discriminante) / (2*a)
+    r2 = (-b - discriminante) / (2*a)
+
+    
+    if r1.imag == 0 and r2.imag == 0:
+        r1 = r1.real
+        r2 = r2.real
+        if r1 == r2:
+            return f"({r1})"
+        else:
+            return f"({r1}, {r2})"
+    else:
+        return "( )"  
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    return a * x**2 + b * x + c
+
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
-
+     if a != 0 and b != 0 and c != 0:
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
+    elif a != 0 and b != 0 and c == 0:
+        return f"f(x) = {a} * X^2 + {b} * X"
+    elif a != 0 and b == 0 and c != 0:
+        return f"f(x) = {a} * X^2 + {c}"
+    elif a != 0 and b == 0 and c == 0:
+        return f"f(x) = {a} * X^2"
+    elif a == 0 and b != 0 and c != 0:
+        return f"f(x) = {b} * X + {c}"
+    elif a == 0 and b != 0 and c == 0:
+        return f"f(x) = {b} * X"
+    elif a == 0 and b == 0 and c != 0:
+        return f"f(x) = {c}"
+    else:
+        return "f(x) = 0
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    if a != 0 and b != 0:
+        return f"f'(x) = {2*a} * X + {b}"
+    elif a != 0 and b == 0:
+        return f"f'(x) = {2*a} * X"
+    elif a == 0 and b != 0:
+        return f"f'(x) = {b}"
+    else:
+        return "f'(x) = 0"
+
